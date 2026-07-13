8226b2b922cf50d3559045a2620333fff4d24112
soheil pakgohar <soheilpakgohar@gmail.com>
2026-07-13 09:25:07 +0330

    bug fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-          integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3w=="
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
